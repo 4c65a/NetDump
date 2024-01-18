69cb6a55da5166d814afa78cdf56b8922075333d
Lean-b <pentestinglean@gmail.com>
2024-01-18 11:00:08 -0300

    Get Headers

--- src/capture/packet.rs
+++ src/capture/packet.rs
@@ -1,46 +1,61 @@
 use pnet::packet::{
-    icmp::IcmpPacket,
-    ipv4::Ipv4Packet,
-    ipv6::Ipv6Packet,
-    tcp::TcpPacket,
-    udp::UdpPacket,
-    Packet,
+    icmp::IcmpPacket, ipv4::Ipv4Packet, ipv6::Ipv6Packet, tcp::TcpPacket, udp::UdpPacket, Packet,
 };
 
 /// Get Headers
 pub trait HeaderDataIpv4<'a> {
     /// Puerto de origen del paquete
     fn get_source(&'a self) -> String;
-    /// Puerto de destino del paquete 
+    /// Puerto de destino del paquete
     fn get_destinations(&'a self) -> String;
     /// Contiene los datos de la aplicación que se están enviando, la carga util del paquete
     fn get_payload(&'a self) -> &[u8];
     /// Contiene un conjunto de banderas de control que se utilizan Ipv4 y Tcp para indicar el estado de la conexión TCP
     fn get_flags(&'a self) -> String;
-    /// Tamaño total del paquete, en bytes Udp
-    fn get_length(&'a self) -> String;
     /// Indica la version del protocolo Ipv4 y Ipv6
     fn get_version(&'a self) -> String;
     /// Time to Live (TTL) Ipv4
     fn get_ttl(&'a self) -> String;
-    /// Indica el tipo de mensaje ICMP que se está enviando.
-    fn get_icmp_type(&'a self) -> String;
 }
 
-pub trait HeaderDataIpv6<'a>{
-    
+pub trait HeaderDataIpv6<'a> {
+    /// Puerto de origen del paquete
+    fn get_source(&'a self) -> String;
+    /// Puerto de destino del paquete
+    fn get_destinations(&'a self) -> String;
+    /// Contiene los datos de la aplicación que se están enviando, la carga util del paquete
+    fn get_payload(&'a self) -> &[u8];
+    /// Indica la version del protocolo Ipv4 y Ipv6
+    fn get_version(&'a self) -> String;
 }
 
 pub trait HeaderDataTcp<'a> {
-    
+    /// Puerto de origen del paquete
+    fn get_source(&'a self) -> String;
+    /// Puerto de destino del paquete
+    fn get_destinations(&'a self) -> String;
+    /// Contiene los datos de la aplicación que se están enviando, la carga util del paquete
+    fn get_payload(&'a self) -> &[u8];
+    /// Contiene un conjunto de banderas de control que se utilizan Ipv4 y Tcp para indicar el estado de la conexión TCP
+    fn get_flags(&'a self) -> String;
 }
 
 pub trait HeaderDataUdp<'a> {
-    
+    /// Puerto de origen del paquete
+    fn get_source(&'a self) -> String;
+    /// Puerto de destino del paquete
+    fn get_destinations(&'a self) -> String;
+    /// Contiene los datos de la aplicación que se están enviando, la carga util del paquete
+    fn get_payload(&'a self) -> &[u8];
+    /// Tamaño total del paquete, en bytes Udp
+    fn get_length(&'a self) -> String;
 }
 
 pub trait HeaderDataIcmp<'a> {
-    
+    /// Indica el tipo de mensaje ICMP que se está enviando.
+    fn get_icmp_type(&'a self) -> String;
+    /// Contiene los datos de la aplicación que se están enviando, la carga util del paquete
+    fn get_payload(&'a self) -> String;
 }
 
 impl<'a> NetworkData<'a> for Ipv4Packet<'a> {
@@ -75,7 +90,6 @@ impl<'a> NetworkData<'a> for Ipv6Packet<'a> {
     fn get_payload(&'a self) -> &[u8] {
         self.payload()
     }
-
 }
 
 impl<'a> NetworkData<'a> for TcpPacket<'a> {
@@ -90,7 +104,6 @@ impl<'a> NetworkData<'a> for TcpPacket<'a> {
     fn get_payload(&'a self) -> &[u8] {
         self.payload()
     }
-
 }
 
 impl<'a> NetworkData<'a> for UdpPacket<'a> {
@@ -105,7 +118,6 @@ impl<'a> NetworkData<'a> for UdpPacket<'a> {
     fn get_payload(&'a self) -> &[u8] {
         self.payload()
     }
-
 }
 
 impl<'a> NetworkData<'a> for IcmpPacket<'a> {
