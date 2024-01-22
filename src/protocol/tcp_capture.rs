76fe356b37811c3826d9942907ceee26c0f8f701
Lean-b <pentestinglean@gmail.com>
2024-01-22 18:49:19 -0300

    Packet

--- src/protocol/tcp_capture.rs
+++ src/protocol/tcp_capture.rs
@@ -1,6 +1,19 @@
-use pnet::packet::tcp::TcpPacket;
+use super::packet::{HeaderDataTcp, HeaderDataIpv4};
+use log::{info, warn};
+
+pub fn tcp_handler(packet: &dyn HeaderDataIpv4<'2>) -> bool {
+    let tcp = pnet::packet::tcp::TcpPacket::new(packet.get_payload());
 
 
-pub fn tcp_handle(packet: &){
-    let tcp = TcpPacket
+    let source_ip = packet.get_source();
+    let destination_ip = packet.get_destinations();
+    let flags_connection = packet.get_flags();
+
+    
+    if let Some(tcp) = tcp{
+        info!("Source: {:?} | Destination: {} | flag: {} | Protocol: TCP",source_ip,destination_ip,flags_connection);
+    } else {
+        warn!("No se esta recibiendo ningun paquete");
+    }
+    true
 }
