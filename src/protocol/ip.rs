4c2b47fb6154f340bcd94be059b75ad3ab6cb805
Lean-b <pentestinglean@gmail.com>
2024-01-24 14:16:46 -0300

    ipv4

--- src/protocol/ip.rs
+++ src/protocol/ip.rs
@@ -32,7 +32,26 @@ fn ipv4(ether: &EthernetPacket, headertcp: &'static dyn HeaderDataTcp, headeripv
                             version
                         );
         } else if packet.get_next_level_protocol() == IpNextHeaderProtocols::Udp {
-            
+               let source_ipv4 = headeripv4.get_source();
+               let destination = headertcp.get_destinations();
+               let destination_ipv4 = headeripv4.get_destinations();
+               let flag = headertcp.get_flags();
+               let flag_ipv4 = headeripv4.get_flags();
+               let payload = headertcp.get_payload();
+               let ttl = headeripv4.get_ttl();
+               let version = headeripv4.get_version();
+               info!("Source: {}|Ipv4{} | Destination: {}|Ipv4{} | Flag
+     : {}|Ipv4{} | Payload: {:#?} | ttl: {} | Version: {} ",
+                               source,
+                               source_ipv4,
+                               destination,
+                               destination_ipv4,
+                               flag,
+                               flag_ipv4,
+                               payload,
+                               ttl,
+                               version
+                           );
         }
     }
 }
