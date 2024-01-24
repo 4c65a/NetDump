21d87bedd2b0cce7863c460e6a6606d0bfff959d
Lean-b <pentestinglean@gmail.com>
2024-01-24 14:39:05 -0300

    ipv4

--- src/protocol/ip.rs
+++ src/protocol/ip.rs
@@ -61,25 +61,24 @@ fn ipv4(
                 version
             );
         } else if packet.get_next_level_protocol() == IpNextHeaderProtocols::Icmp {
-              let icmp_type = headericmp.get_icmp_types();
-              let destination_ipv4 = headeripv4.get_destinations();
-              let flag_ipv4 = headeripv4.get_flags();
-              let payload = headericmp.get_payload()
-              let ttl = headeripv4.get_ttl();
-              let version = headeripv4.get_version();
-              info!(
-                  "Source: {}|Ipv4{} | Destination: {}|Ipv4{} | Flag
-       : {}|Ipv4{} | Payload: {:#?} | ttl: {} | Version: {} ",
-                  source,
-                  source_ipv4,
-                  destination,
-                  destination_ipv4,
-                  length,
-                  flag_ipv4,
-                  payload,
-                  ttl,
-                  version
-             );
+            let source_ipv4 = headeripv4.get_source();
+            let icmp_type = headericmp.get_icmp_types();
+            let destination_ipv4 = headeripv4.get_destinations();
+            let flag_ipv4 = headeripv4.get_flags();
+            let payload = headericmp.get_payload();
+            let ttl = headeripv4.get_ttl();
+            let version = headeripv4.get_version();
+            info!(
+                "Source Ipv4:{} | IcmpType: {:#?}| Destination Ipv4: {} | Flag
+       : {} | Payload: {:#?} | ttl: {} | Version: {} ",
+                source_ipv4,
+                icmp_type,
+                destination_ipv4,
+                flag_ipv4,
+                payload,
+                ttl,
+                version
+            );
         }
     }
 }
