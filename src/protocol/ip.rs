ec790862f65bf7798c61a51a9b5c24c4ada8f488
Lean-b <pentestinglean@gmail.com>
2024-01-23 23:41:48 -0300

    ipv4

--- src/protocol/ip.rs
+++ src/protocol/ip.rs
@@ -1,6 +1,6 @@
 use pnet::packet::{ethernet::{EthernetPacket,EtherTypes}, ip::IpNextHeaderProtocols, ipv4::Ipv4Packet, tcp::TcpPacket, Packet};
 use super::packet::{HeaderDataIpv4, HeaderDataTcp};
-use log;
+use log::{self, info};
 
 fn ipv4(ether: &EthernetPacket, headertcp: &dyn HeaderDataTcp){
     match ether.get_ethertype() {
@@ -8,8 +8,11 @@ fn ipv4(ether: &EthernetPacket, headertcp: &dyn HeaderDataTcp){
             if let Some(packet) = Ipv4Packet::new(ether.payload()) {
                 match packet.get_next_level_protocol() {
                     IpNextHeaderProtocols::Tcp => {
-                        
-                        
+                        let source = headertcp.get_source();
+                        let destination = headertcp.get_destinations();
+                        let flag = headertcp.get_flags();
+                        let payload = headertcp.get_payload();
+                        info!();
                             
                     }
                 }
