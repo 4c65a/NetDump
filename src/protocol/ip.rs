58a90960d7c5c6270ea1e5524d4871e386946378
Lean-b <pentestinglean@gmail.com>
2024-01-24 14:21:15 -0300

    ipv4

--- src/protocol/ip.rs
+++ src/protocol/ip.rs
@@ -7,7 +7,13 @@ use pnet::packet::{
     Packet,
 };
 
-fn ipv4(ether: &EthernetPacket, headertcp: &'static dyn HeaderDataTcp, headeripv4: &'static dyn HeaderDataIpv4, headerudp: &'static dyn HeaderDataUdp, headericmp &'static dyn HeaderDataIcmp) {
+fn ipv4(
+    ether: &EthernetPacket,
+    headertcp: &'static dyn HeaderDataTcp,
+    headeripv4: &'static dyn HeaderDataIpv4,
+    headerudp: &'static dyn HeaderDataUdp,
+    headericmp: &'static dyn HeaderDataIcmp,
+) {
     if ether.get_ethertype() == EtherTypes::Ipv4 {
         let packet = Ipv4Packet::new(ether.payload()).unwrap();
         if packet.get_next_level_protocol() == IpNextHeaderProtocols::Tcp {
@@ -32,26 +38,28 @@ fn ipv4(ether: &EthernetPacket, headertcp: &'static dyn HeaderDataTcp, headeripv
                             version
                         );
         } else if packet.get_next_level_protocol() == IpNextHeaderProtocols::Udp {
-               let source_ipv4 = headeripv4.get_source();
-               let destination = headertcp.get_destinations();
-               let destination_ipv4 = headeripv4.get_destinations();
-               let flag = headertcp.get_flags();
-               let flag_ipv4 = headeripv4.get_flags();
-               let payload = headertcp.get_payload();
-               let ttl = headeripv4.get_ttl();
-               let version = headeripv4.get_version();
-               info!("Source: {}|Ipv4{} | Destination: {}|Ipv4{} | Flag
+            let source = headerudp.get_source();
+            let source_ipv4 = headeripv4.get_source();
+            let destination = headerudp.get_destinations();
+            let destination_ipv4 = headeripv4.get_destinations();
+            let flag_ipv4 = headeripv4.get_flags();
+            let payload = headerudp.get_payload();
+            let length = headerudp.get_length();
+            let ttl = headeripv4.get_ttl();
+            let version = headeripv4.get_version();
+            info!(
+                "Source: {}|Ipv4{} | Destination: {}|Ipv4{} | Flag
      : {}|Ipv4{} | Payload: {:#?} | ttl: {} | Version: {} ",
-                               source,
-                               source_ipv4,
-                               destination,
-                               destination_ipv4,
-                               flag,
-                               flag_ipv4,
-                               payload,
-                               ttl,
-                               version
-                           );
+                source,
+                source_ipv4,
+                destination,
+                destination_ipv4,
+                length,
+                flag_ipv4,
+                payload,
+                ttl,
+                version
+            );
         }
     }
 }
