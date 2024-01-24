99c372aa8059ba31cc15ec7cb5fd43ba7adc1e12
Lean-b <pentestinglean@gmail.com>
2024-01-24 17:39:50 -0300

    ipv6

--- src/protocol/ip.rs
+++ src/protocol/ip.rs
@@ -1,7 +1,11 @@
 use super::packet::{HeaderDataIcmp, HeaderDataIpv4, HeaderDataIpv6, HeaderDataTcp, HeaderDataUdp};
 use log::{self, info};
 use pnet::packet::{
-    ethernet::{EtherTypes, EthernetPacket}, ip::IpNextHeaderProtocols, ipv4::Ipv4Packet, ipv6::Ipv6Packet, Packet
+    ethernet::{EtherTypes, EthernetPacket},
+    ip::IpNextHeaderProtocols,
+    ipv4::Ipv4Packet,
+    ipv6::Ipv6Packet,
+    Packet,
 };
 
 fn ipv4(
@@ -23,7 +27,7 @@ fn ipv4(
             let payload = headertcp.get_payload();
             let ttl = headeripv4.get_ttl();
             let version = headeripv4.get_version();
-            info!("Source: {}|Ipv4{} | Destination: {}|Ipv4{} | Flag: {}|Ipv4{} | Payload: {:#?} | ttl: {} | Version: {} ",
+            info!("Source: {} | Ipv4: {} | Destination: {} | Ipv4: {} | Flag: {} | Ipv4: {} | Payload: {:#?} | ttl: {} | Version: {} ",
                             source,
                             source_ipv4,
                             destination,
@@ -45,8 +49,8 @@ fn ipv4(
             let ttl = headeripv4.get_ttl();
             let version = headeripv4.get_version();
             info!(
-                "Source: {}|Ipv4{} | Destination: {}|Ipv4{} | Flag
-     : {}|Ipv4{} | Payload: {:#?} | ttl: {} | Version: {} ",
+                "Source: {} | Ipv4: {} | Destination: {} | Ipv4: {} | Flag
+     : {} | Ipv4 {} | Payload: {:#?} | ttl: {} | Version: {} ",
                 source,
                 source_ipv4,
                 destination,
@@ -91,52 +95,42 @@ fn ipv6(
             let flag = headertcp.get_flags();
             let payload = headertcp.get_payload();
             let version = headeripv6.get_version();
-            info!("Source: {}|Ipv4{} | Destination: {}|Ipv4{} | Flag: {}|Ipv4{} | Payload: {:#?} | ttl: {} | Version: {} ",
+            info!("Source: {}| Ipv6: {} | Destination: {} | Ipv6: {} | flag: {} | Payload: {:#?} | Version: {} ",
                             source,
-                            source_ipv4,
+                            source_ipv6,
                             destination,
-                            destination_ipv4,
+                            destination_ipv6,
                             flag,
-                            flag_ipv4,
                             payload,
-                            ttl,
                             version
                         );
         } else if packet.get_next_header() == IpNextHeaderProtocols::Udp {
             let source = headerudp.get_source();
-            let source_ipv4 = headeripv4.get_source();
+            let source_ipv6 = headeripv6.get_source();
             let destination = headerudp.get_destinations();
-            let destination_ipv4 = headeripv4.get_destinations();
-            let flag_ipv4 = headeripv4.get_flags();
+            let destination_ipv6 = headeripv6.get_destinations();
             let payload = headerudp.get_payload();
             let length = headerudp.get_length();
-            let ttl = headeripv4.get_ttl();
-            let version = headeripv4.get_version();
+            let version = headeripv6.get_version();
             info!(
-                "Source: {}|Ipv4{} | Destination: {}|Ipv4{} | Flag
-     : {}|Ipv4{} | Payload: {:#?} | ttl: {} | Version: {} ",
+                "Source: {}| Ipv6: {} | Destination: {}| Ipv6: {} | Length: {} | Payload: {:#?} | Version: {} ",
                 source,
-                source_ipv4,
+                source_ipv6,
                 destination,
-                destination_ipv4,
+                destination_ipv6,
                 length,
-                flag_ipv4,
                 payload,
-                ttl,
                 version
             );
         } else if packet.get_next_header() == IpNextHeaderProtocols::Icmp {
-            let source_ipv4 = headeripv4.get_source();
+            let source_ipv6 = headeripv6.get_source();
             let icmp_type = headericmp.get_icmp_types();
-            let destination_ipv4 = headeripv4.get_destinations();
-            let flag_ipv4 = headeripv4.get_flags();
+            let destination_ipv6 = headeripv6.get_destinations();
             let payload = headericmp.get_payload();
-            let ttl = headeripv4.get_ttl();
-            let version = headeripv4.get_version();
+            let version = headeripv6.get_version();
             info!(
-                "Source Ipv4:{} | IcmpType: {:#?}| Destination Ipv4: {} | Flag
-       : {} | Payload: {:#?} | ttl: {} | Version: {} ",
-                source_ipv4, icmp_type, destination_ipv4, flag_ipv4, payload, ttl, version
+                "Source Ipv6:{} | IcmpType: {:#?}| Destination Ipv6: {} | Payload: {:#?} | Version: {} ",
+                source_ipv6, icmp_type, destination_ipv6, payload, version
             );
         }
     }
