601d7ef3ec771510b316aef5799a33629430fc26
Lean-b <pentestinglean@gmail.com>
2024-01-23 23:46:53 -0300

    ipv4

--- src/protocol/ip.rs
+++ src/protocol/ip.rs
@@ -1,18 +1,23 @@
-use pnet::packet::{ethernet::{EthernetPacket,EtherTypes}, ip::IpNextHeaderProtocols, ipv4::Ipv4Packet, tcp::TcpPacket, Packet};
+use pnet::packet::{ethernet::{EthernetPacket,EtherTypes}, ip::IpNextHeaderProtocols, ipv4::Ipv4Packet, Packet};
 use super::packet::{HeaderDataIpv4, HeaderDataTcp};
 use log::{self, info};
 
-fn ipv4(ether: &EthernetPacket, headertcp: &dyn HeaderDataTcp){
+fn ipv4(ether: &EthernetPacket, headertcp: &dyn HeaderDataTcp, headeripv4: &dyn HeaderDataIpv4){
     match ether.get_ethertype() {
         EtherTypes::Ipv4 => {
             if let Some(packet) = Ipv4Packet::new(ether.payload()) {
                 match packet.get_next_level_protocol() {
                     IpNextHeaderProtocols::Tcp => {
                         let source = headertcp.get_source();
+                        let source_ipv4 = headeripv4.get_source(); 
                         let destination = headertcp.get_destinations();
+                        let destination_ipv4 = headeripv4.get_destinations();
                         let flag = headertcp.get_flags();
+                        let flag_ipv4 = headeripv4.get_flags();
                         let payload = headertcp.get_payload();
-                        info!();
+                        let ttl = headeripv4.get_ttl();
+                        let version = headeripv4.get_version();
+                        
                             
                     }
                 }
