07f2d1e14e19fcbf1d514cdcd7ac01fc2c9ab932
Lean-b <pentestinglean@gmail.com>
2024-01-24 21:18:21 -0300

    cmd.rs Command

--- src/cmd.rs
+++ src/cmd.rs
@@ -1,9 +1,9 @@
-use clap::{self, Command};
+use clap::{Arg, Command};
 
-pub struct Cmd {
-    
-}
-
-impl <a>Cmd for Command<a> {
-    
+pub fn cmd() {
+    let matches = Command::new("netdump")
+        .author("Leandro <Kirov>")
+        .version("v0.1.0")
+        .about("Packet capturer, you can capture packets from three different protocols")
+        .arg(Arg::new().short().long().index().help().required(true));
 }
