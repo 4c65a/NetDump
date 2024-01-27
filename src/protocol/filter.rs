9c3375dd29e0763f01b5f2d7a77cc050fea63aaa
Lean-b <pentestinglean@gmail.com>
2024-01-26 22:53:44 -0300

    Filter port filter.rs

--- src/protocol/filter.rs
+++ src/protocol/filter.rs
@@ -1,14 +1,2 @@
-use pnet::packet::{ethernet::EthernetPacket, icmp::Icmp, tcp::Tcp, udp::Udp};
 
-/// Struct for filter protocol
-pub struct Filter {
-    tcp: Tcp,
-    udp: Udp,
-    icmp: Icmp
-}
-
-impl Filter {
-    fn filter_protocol(){
-        
-    }
-}
+pub fn filter_protocol(filter: &str) {}
