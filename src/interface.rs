86aa22a5da39e0f36e72a1b5b7d7975051cc1ec8
Lean-b <pentestinglean@gmail.com>
2024-01-28 10:58:28 -0300

    interface.rs

--- src/interface.rs
+++ src/interface.rs
@@ -2,7 +2,10 @@ use pnet::datalink::interfaces;
 
 pub fn interface(int_name: &str){
     let interface = interfaces();
-    interface.into_iter().filter(|inter| inter.name == *int_name ).next().expect("Failed to get interface");
+    let inter = interface.into_iter().filter(|inter| inter.name == *int_name ).next().expect("Failed to get interface");
 
+    match inter {
+        
+    }
     
 }
