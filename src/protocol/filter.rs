3aa7c5e9cf0b999757e282ee1230ca3b1461bec1
Lean-b <pentestinglean@gmail.com>
2024-01-30 14:18:12 -0300

    interface list system

--- src/protocol/filter.rs
+++ src/protocol/filter.rs
@@ -1,5 +1,12 @@
 
+use pnet::datalink::interfaces;
 
-pub fn filter_protocol(filter: &str) {
-    let inter = 
+pub fn index_interface() {
+    let interfaces = interfaces();
+    let filtered_interfaces = interfaces.iter().filter(|e| e.is_up() && !e.ips.is_empty());
+
+    println!("List of network interfaces on your system:");
+    for inter in filtered_interfaces{
+        println!("- {}",inter.name);
+    }
 }
