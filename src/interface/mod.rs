521b1315f7cc3cad0605acd42af30d38e312aa26
Lean-b <pentestinglean@gmail.com>
2024-02-23 19:56:40 -0300

    Command

--- src/interface/mod.rs
+++ src/interface/mod.rs
@@ -1,2 +1,2 @@
-pub mod interface;
 pub mod list_interface;
+pub mod network_interfaces;
--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,44 @@
 use crate::interface::list_interface;
-use clap::{Arg, Command};
-use log::info;
+use std::env;
+//use log::info;
 mod interface;
 mod protocol;
 
 fn main() {
-    // let matches = cmd().get_matches_from(std::env::args());
-    let _matches = Command::new("NetDump")
-        .version("0.0.1")
-        .about("Capture networks packet")
-        .author("Leandro")
-        .arg(
-            Arg::new("list")
-                .short('l')
-                .long("list")
-                .help("List of system interfaces"),
-        )
-        .subcommand(
-            Command::new("interface")
-                .about("Sets the network interface for packet capture")
-                .arg(
-                    Arg::new("interface")
-                        .short('i')
-                        .long("interface")
-                        .value_name("INTERFACE")
-                        .help("Specifies the network interface"),
-                ),
-        )
-        .get_matches();
+    // Parse command-line arguments using a dedicated library
+    let args: Vec<String> = env::args().collect();
 
-    match _matches.subcommand() {
-        Some(("list", _)) => {
-            let index = list_interface::index_interface();
-            for name in index {
-                println!("- {}", name);
-            }
+    // Handle --list argument
+    if args.contains(&"--list".to_string()) {
+        let index = list_interface::index_interface();
+        for name in index {
+            println!("- {}", name);
         }
-        Some(("interface", m)) => {
-            let int_name = m;
-            interface::interface::interface(int_name);
-        }
-        _ => info!("Clap don't command"),
-    };
+    } else if args.len() >= 3 && args[1] == "--interface" {
+        // Access interface name if provided after --interface
+        let int_name = &args[2];
+
+        // Call the interface function to handle packet capturing
+        interface::network_interfaces::interface(int_name);
+    } else {
+        // Handle missing or invalid arguments
+        println!("Usage: netdump [--list] [--interface <interface_name>]");
+    }
+
+    // match matches.subcommand() {
+    //     Some(("list", _)) => {
+    //         let index = list_interface::index_interface();
+    //          for name in index {
+    //             println!("- {}", name);
+    //         }
+    //     }
+    //     Some(("interface", m)) => {
+    //         if let Some(int_name) = m.get_one::<String>("interface") {
+    //             interface::network_interfaces::interface(int_name.as_str());
+    //         } else {
+    //              println!("No interface specified");
+    //         }
+    //     }
+    //     _ => info!("Clap don't command"),
+    // };
 }
