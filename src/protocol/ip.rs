dea9c346ad5b02bb903519eee8dd9cfe68007f11
Lean-b <pentestinglean@gmail.com>
2024-02-24 18:13:26 -0300

    Modifier ip.rs

--- src/protocol/ip.rs
+++ src/protocol/ip.rs
@@ -8,13 +8,143 @@ use pnet::packet::{
     Packet,
 };
 
+pub fn ipv4_handler(
+    ether: &EthernetPacket, // headertcp: &'static dyn HeaderDataTcp,
+                            // headeripv4: &'static dyn HeaderDataIpv4,
+                            // headerudp: &'static dyn HeaderDataUdp,
+                            // headericmp: &'static dyn HeaderDataIcmp,
+) {
+    if ether.get_ethertype() == EtherTypes::Ipv4 {
+        let packet = Ipv4Packet::new(ether.payload()).unwrap();
+        // Handler TCP
+        if packet.get_next_level_protocol() == IpNextHeaderProtocols::Tcp {
+            let source =  ;
+            let source_ipv4 = headeripv4.get_source();
+            let destination = headertcp.get_destinations();
+            let destination_ipv4 = headeripv4.get_destinations();
+            let flag = headertcp.get_flags();
+            let flag_ipv4 = headeripv4.get_flags();
+            let payload = headertcp.get_payload();
+            let ttl = headeripv4.get_ttl();
+            let version = headeripv4.get_version();
+            info!("Source: {}|Ipv4{} | Destination: {}|Ipv4{} | Flag: {}|Ipv4{} | Payload: {:#?} | ttl: {} | Version: {} ",
+                            source,
+                            source_ipv4,
+                            destination,
+                            destination_ipv4,
+                            flag,
+                            flag_ipv4,
+                            payload,
+                            ttl,
+                            version
+                        );
+        // Handler UDP    
+        } else if packet.get_next_level_protocol() == IpNextHeaderProtocols::Udp {
+            let source = headerudp.get_source();
+            let source_ipv4 = headeripv4.get_source();
+            let destination = headerudp.get_destinations();
+            let destination_ipv4 = headeripv4.get_destinations();
+            let flag_ipv4 = headeripv4.get_flags();
+            let payload = headerudp.get_payload();
+            let length = headerudp.get_length();
+            let ttl = headeripv4.get_ttl();
+            let version = headeripv4.get_version();
+            info!(
+                "Source: {}|Ipv4{} | Destination: {}|Ipv4{} | Flag
+     : {}|Ipv4{} | Payload: {:#?} | ttl: {} | Version: {} ",
+                source,
+                source_ipv4,
+                destination,
+                destination_ipv4,
+                length,
+                flag_ipv4,
+                payload,
+                ttl,
+                version
+            );
+        // Handler ICMP 
+        } else if packet.get_next_level_protocol() == IpNextHeaderProtocols::Icmp {
+            let source_ipv4 = headeripv4.get_source();
+            let icmp_type = headericmp.get_icmp_types();
+            let destination_ipv4 = headeripv4.get_destinations();
+            let flag_ipv4 = headeripv4.get_flags();
+            let payload = headericmp.get_payload();
+            let ttl = headeripv4.get_ttl();
+            let version = headeripv4.get_version();
+            info!(
+                "Source Ipv4:{} | IcmpType: {:#?}| Destination Ipv4: {} | Flag
+       : {} | Payload: {:#?} | ttl: {} | Version: {} ",
+                source_ipv4, icmp_type, destination_ipv4, flag_ipv4, payload, ttl, version
+            );
+        }
+    }
+}
+pub fn ipv6_handler(
+    ether: &EthernetPacket,
+    headertcp: &'static dyn HeaderDataTcp,
+    headeripv6: &'static dyn HeaderDataIpv6,
+    headerudp: &'static dyn HeaderDataUdp,
+    headericmp: &'static dyn HeaderDataIcmp,
+) {
+    if ether.get_ethertype() == EtherTypes::Ipv6 {
+        let packet = Ipv6Packet::new(ether.payload()).unwrap();
+        if packet.get_next_header() == IpNextHeaderProtocols::Tcp {
+            let source = headertcp.get_source();
+            let source_ipv6 = headeripv6.get_source();
+            let destination = headertcp.get_destinations();
+            let destination_ipv6 = headeripv6.get_destinations();
+            let flag = headertcp.get_flags();
+            let payload = headertcp.get_payload();
+            let version = headeripv6.get_version();
+            info!("Source: {}| Ipv6: {} | Destination: {} | Ipv6: {} | flag: {} | Payload: {:#?} | Version: {} ",
+                            source,
+                            source_ipv6,
+                            destination,
+                            destination_ipv6,
+                            flag,
+                            payload,
+                            version
+                        );
+        } else if packet.get_next_header() == IpNextHeaderProtocols::Udp {
+            let source = headerudp.get_source();
+            let source_ipv6 = headeripv6.get_source();
+            let destination = headerudp.get_destinations();
+            let destination_ipv6 = headeripv6.get_destinations();
+            let payload = headerudp.get_payload();
+            let length = headerudp.get_length();
+            let version = headeripv6.get_version();
+            info!(
+                "Source: {}| Ipv6: {} | Destination: {}| Ipv6: {} | Length: {} | Payload: {:#?} | Version: {} ",
+                source,
+                source_ipv6,
+                destination,
+                destination_ipv6,
+                length,
+                payload,
+                version
+            );
+        } else if packet.get_next_header() == IpNextHeaderProtocols::Icmp {
+            let source_ipv6 = headeripv6.get_source();
+            let icmp_type = headericmp.get_icmp_types();
+            let destination_ipv6 = headeripv6.get_destinations();
+            let payload = headericmp.get_payload();
+            let version = headeripv6.get_version();
+            info!(
+                "Source Ipv6:{} | IcmpType: {:#?}| Destination Ipv6: {} | Payload: {:#?} | Version: {} ",
+                source_ipv6, icmp_type, destination_ipv6, payload, version
+            );
+        }
+    }
+}
+/*
 /// Global variables
 static mut HEADER_TCP: Option<&'static dyn HeaderDataTcp> = None;
 static mut HEADER_IPV4: Option<&'static dyn HeaderDataIpv4> = None;
 static mut HEADER_IPV6: Option<&'static dyn HeaderDataIpv6> = None;
 static mut HEADER_UDP: Option<&'static dyn HeaderDataUdp> = None;
 static mut HEADER_ICMP: Option<&'static dyn HeaderDataIcmp> = None;
-
+*/
+/*
 pub fn ipv4_handler(ether: &EthernetPacket) {
     if ether.get_ethertype() == EtherTypes::Ipv4 {
         let packet = Ipv4Packet::new(ether.payload()).unwrap();
@@ -28,7 +158,7 @@ pub fn ipv4_handler(ether: &EthernetPacket) {
                 let flag_ipv4 = HEADER_IPV4.unwrap().get_flags();
                 let payload = HEADER_TCP.unwrap().get_payload();
                 let ttl = HEADER_IPV4.unwrap().get_ttl();
-                let version = HEADER_IPV4.unwrap().get_version();
+                let version = HEADER_IPV4.unwrap_or_else().get_version();
                 info!("Source: {} | Ipv4: {} | Destination: {} | Ipv4: {} | Flag: {} | Ipv4: {} | Payload: {:#?} | ttl: {} | Version: {} ",
                             source,
                             source_ipv4,
@@ -142,3 +272,4 @@ pub fn ipv6_handler(ether: &EthernetPacket) {
         }
     }
 }
+*/
