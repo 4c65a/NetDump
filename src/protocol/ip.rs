9b4d162cbf6d771abdc7fe5bd008e0f237ed7bdd
Lean-b <pentestinglean@gmail.com>
2024-03-01 12:04:48 -0300

    Banner

--- src/protocol/ip.rs
+++ src/protocol/ip.rs
@@ -1,4 +1,3 @@
-//use super::packet::{HeaderDataIcmp, HeaderDataIpv4, HeaderDataIpv6, HeaderDataTcp, HeaderDataUdp};
 use pnet::packet::{
     ethernet::{EtherTypes, EthernetPacket},
     icmp::IcmpPacket,
@@ -10,9 +9,7 @@ use pnet::packet::{
     Packet,
 };
 
-use crate::protocol::packet::{
-    HeaderDataIcmp, HeaderDataIpv4, HeaderDataIpv6, HeaderDataTcp, HeaderDataUdp,
-};
+//use crate::protocol::packet::{HeaderDataIcmp, HeaderDataIpv4, HeaderDataIpv6, HeaderDataUdp};
 
 pub fn ipv4_handler(ether: &EthernetPacket) {
     if ether.get_ethertype() == EtherTypes::Ipv4 {
@@ -28,57 +25,46 @@ pub fn ipv4_handler(ether: &EthernetPacket) {
             let destination_ipv4 = packet.get_destination();
             let flag = tcp.get_flags();
             let flag_ipv4 = packet.get_flags();
-            let payload = tcp.get_payload();
+            //let payload = tcp.get_payload();
             let ttl = packet.get_ttl();
             let version = packet.get_version();
-            println!("Source:{} | Ipv4 {}| Destination:{} | Ipv4 {} | Flag:{} | Ipv4 {} | Payload: {:#?} | ttl: {} | Version: {} ",
+            println!("Protocol: TCP | Source:{} | Ipv4 {}| Destination:{} | Ipv4 {} | Flag:{} | Ipv4 {} | ttl: {} | Version: {} ",
                             source,
                             source_ipv4,
                             destination,
                             destination_ipv4,
                             flag,
                             flag_ipv4,
-                            payload,
-                            ttl,
+                             ttl,
                             version
                         );
         // Handler UDP
         } else if packet.get_next_level_protocol() == IpNextHeaderProtocols::Udp {
             let source = udp.get_source();
             let source_ipv4 = packet.get_source();
-            let destination = udp.get_destinations();
-            let destination_ipv4 = packet.get_destinations();
+            let destination = udp.get_destination();
+            let destination_ipv4 = packet.get_destination();
             let flag_ipv4 = packet.get_flags();
-            let payload = udp.get_payload();
+            //let payload = udp.get_payload();
             let length = udp.get_length();
             let ttl = packet.get_ttl();
             let version = packet.get_version();
             println!(
-                "Source: {} | Ipv4 {} | Destination: {} | Ipv4 {} | Flag
-     : {} | Ipv4 {} | Payload: {:#?} | ttl: {} | Version: {} ",
-                source,
-                source_ipv4,
-                destination,
-                destination_ipv4,
-                length,
-                flag_ipv4,
-                payload,
-                ttl,
-                version
+                "Protocol: UDP | Source: {} | Ipv4 {} | Destination: {} | Ipv4 {} | Flag: {} | Ipv4 {} | ttl: {} | Version: {} ",
+                source, source_ipv4, destination, destination_ipv4, length, flag_ipv4, ttl, version
             );
         // Handler ICMP
         } else if packet.get_next_level_protocol() == IpNextHeaderProtocols::Icmp {
             let source_ipv4 = packet.get_source();
-            let icmp_type = icmp.get_icmp_types();
-            let destination_ipv4 = packet.get_destinations();
+            let icmp_type = icmp.get_icmp_type();
+            let destination_ipv4 = packet.get_destination();
             let flag_ipv4 = packet.get_flags();
-            let payload = icmp.get_payload();
+            //let payload = icmp.get_payload();
             let ttl = packet.get_ttl();
             let version = packet.get_version();
             println!(
-                "Source_Ipv4: {} | IcmpType: {:#?} | Destination Ipv4: {} | Flag
-       : {} | Payload: {:#?} | ttl: {} | Version: {} ",
-                source_ipv4, icmp_type, destination_ipv4, flag_ipv4, payload, ttl, version
+                "Protocol: ICMP | Source_Ipv4: {} | IcmpType: {:#?} | Destination Ipv4: {} | Flag: {} | ttl: {} | Version: {} ",
+                source_ipv4, icmp_type, destination_ipv4, flag_ipv4, ttl, version
             );
         }
     }
@@ -93,46 +79,35 @@ pub fn ipv6_handler(ether: &EthernetPacket) {
             let source = tcp.get_source();
             let source_ipv6 = packet.get_source();
             let destination = tcp.get_destination();
-            let destination_ipv6 = packet.get_destinations();
+            let destination_ipv6 = packet.get_destination();
             let flag = tcp.get_flags();
-            let payload = tcp.get_payload();
+            // let payload = tcp.get_payload();
             let version = packet.get_version();
-            println!("Source: {} | Ipv6: {} | Destination: {} | Ipv6: {} | flag: {} | Payload: {:#?} | Version: {} ",
-                            source,
-                            source_ipv6,
-                            destination,
-                            destination_ipv6,
-                            flag,
-                            payload,
-                            version
-                        );
+            println!(
+                "Source: {} | Ipv6: {} | Destination: {} | Ipv6: {} | flag: {} | Version: {} ",
+                source, source_ipv6, destination, destination_ipv6, flag, version
+            );
         } else if packet.get_next_header() == IpNextHeaderProtocols::Udp {
             let source = udp.get_source();
             let source_ipv6 = packet.get_source();
-            let destination = udp.get_destinations();
-            let destination_ipv6 = packet.get_destinations();
-            let payload = udp.get_payload();
+            let destination = udp.get_destination();
+            let destination_ipv6 = packet.get_destination();
+            //let payload = udp.get_payload();
             let length = udp.get_length();
             let version = packet.get_version();
             println!(
-                "Source: {} | Ipv6: {} | Destination: {} | Ipv6: {} | Length: {} | Payload: {:#?} | Version: {} ",
-                source,
-                source_ipv6,
-                destination,
-                destination_ipv6,
-                length,
-                payload,
-                version
+                "Source: {} | Ipv6: {} | Destination: {} | Ipv6: {} | Length: {} | Version: {} ",
+                source, source_ipv6, destination, destination_ipv6, length, version
             );
         } else if packet.get_next_header() == IpNextHeaderProtocols::Icmp {
             let source_ipv6 = packet.get_source();
             let icmp_type = icmp.get_icmp_type();
-            let destination_ipv6 = packet.get_destinations();
-            let payload = icmp.get_payload();
+            let destination_ipv6 = packet.get_destination();
+            //let payload = icmp.get_payload();
             let version = packet.get_version();
             println!(
-                "Source_Ipv6:{} | IcmpType: {:#?} | Destination Ipv6: {} | Payload: {:#?} | Version: {} ",
-                source_ipv6, icmp_type, destination_ipv6, payload, version
+                "Source_Ipv6:{} | IcmpType: {:#?} | Destination Ipv6: {} | Version: {} ",
+                source_ipv6, icmp_type, destination_ipv6, version
             );
         }
     }
