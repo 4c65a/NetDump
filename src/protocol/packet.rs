86679c061fb6b8c773c5e7ce9fbb323ef7ac44c8
Lean-b <pentestinglean@gmail.com>
2024-03-07 12:11:36 -0300

    Acomode el codigo

--- src/protocol/packet.rs
+++ src/protocol/packet.rs
@@ -1,11 +1,4 @@
-use pnet::packet::{
-    icmp::{IcmpPacket, IcmpType},
-    ipv4::Ipv4Packet,
-    ipv6::Ipv6Packet,
-    tcp::TcpPacket,
-    udp::UdpPacket,
-    Packet,
-};
+use pnet::packet::{ipv4::Ipv4Packet, ipv6::Ipv6Packet, Packet};
 
 /// Get Headers Ipv4
 pub trait HeaderDataIpv4<'a> {
@@ -32,35 +25,6 @@ pub trait HeaderDataIpv6<'a> {
     fn get_payload(&'a self) -> &[u8];
     /// Indica la version del protocolo Ipv4 y Ipv6
     fn get_version(&'a self) -> String;
-}
-/// Get Headers TCP
-pub trait HeaderDataTcp<'a> {
-    /// Puerto de origen del paquete
-    fn get_source(&'a self) -> String;
-    /// Puerto de destino del paquete
-    fn get_destinations(&'a self) -> String;
-    /// Contiene los datos de la aplicación que se están enviando, la carga util del paquete
-    fn get_payload(&'a self) -> &[u8];
-    /// Contiene un conjunto de banderas de control que se utilizan Ipv4 y Tcp para indicar el estado de la conexión TCP
-    fn get_flags(&'a self) -> String;
-}
-/// Get Headers UDP
-pub trait HeaderDataUdp<'a> {
-    /// Puerto de origen del paquete
-    fn get_source(&'a self) -> String;
-    /// Puerto de destino del paquete
-    fn get_destinations(&'a self) -> String;
-    /// Contiene los datos de la aplicación que se están enviando, la carga util del paquete
-    fn get_payload(&'a self) -> &[u8];
-    /// Tamaño total del paquete, en bytes Udp
-    fn get_length(&'a self) -> String;
-}
-/// Get Headers ICMP
-pub trait HeaderDataIcmp<'a> {
-    /// Indica el tipo de mensaje ICMP que se está enviando.
-    fn get_icmp_types(&'a self) -> IcmpType;
-    /// Contiene los datos de la aplicación que se están enviando, la carga util del paquete
-    fn get_payload(&'a self) -> &[u8];
 }
 
 /// Implementacion de Ipv4 para obetner los encabezados
@@ -98,47 +62,5 @@ impl<'a> HeaderDataIpv6<'a> for Ipv6Packet<'a> {
     }
     fn get_version(&'a self) -> String {
         self.get_version().to_string()
-    }
-}
-
-/// Implementacion de Tcp para obtener los encabezados
-impl<'a> HeaderDataTcp<'a> for TcpPacket<'a> {
-    fn get_source(&'a self) -> String {
-        self.get_source().to_string()
-    }
-    fn get_destinations(&'a self) -> String {
-        self.get_destination().to_string()
-    }
-    fn get_payload(&'a self) -> &[u8] {
-        self.payload()
-    }
-    fn get_flags(&'a self) -> String {
-        self.get_flags().to_string()
-    }
-}
-
-/// Implementacion de Udp para obtener los encabezados
-impl<'a> HeaderDataUdp<'a> for UdpPacket<'a> {
-    fn get_source(&'a self) -> String {
-        self.get_source().to_string()
-    }
-    fn get_destinations(&'a self) -> String {
-        self.get_destination().to_string()
-    }
-    fn get_payload(&'a self) -> &[u8] {
-        self.payload()
-    }
-    fn get_length(&'a self) -> String {
-        self.get_length().to_string()
-    }
-}
-
-/// Implementacion de Icmp para obtener los encabezados
-impl<'a> HeaderDataIcmp<'a> for IcmpPacket<'a> {
-    fn get_icmp_types(&'a self) -> IcmpType {
-        self.get_icmp_type()
-    }
-    fn get_payload(&'a self) -> &[u8] {
-        self.payload()
     }
 }
