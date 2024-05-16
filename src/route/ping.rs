caf1356ca72a8c65e542935a8e265dd1ca019704
4c65a <pentestinglean@gmail.com>
2024-05-16 22:39:35 +0000

    .

--- src/route/ping.rs
+++ src/route/ping.rs
@@ -26,39 +26,48 @@
     Output
 */
 
-use pnet::packet::{
-    icmp::{
-        echo_request::{IcmpCodes, MutableEchoRequestPacket},
-        IcmpTypes,
+use std::net::Ipv4Addr;
+
+use pnet::{
+    packet::{
+        icmp::{
+            echo_request::{IcmpCodes, MutableEchoRequestPacket},
+            IcmpTypes,
+        },
+        ip::IpNextHeaderProtocols::{self, Ipv4},
+        ipv4::Ipv4Flags, MutableIpv4Packet},
+        util, Packet,
     },
-    ip::IpNextHeaderProtocols,
-    ipv4::MutableIpv4Packet,
-    util, Packet,
+    transport,
 };
 use rand::random;
 
-fn create_packet_ipv4<'a>(header: &'a mut [u8]) -> Result<MutableIpv4Packet<'a>, std::io::Error> {
+fn create_packet_ipv4<'a>(
+    header: &'a mut [u8],
+    destination: Ipv4Addr,
+) -> Result<MutableIpv4Packet<'a>, std::io::Error> {
     let mut ipv4_packet = MutableIpv4Packet::new(header).unwrap();
-    ipv4_packet.set_version();
-    ipv4_packet.set_header_length();
-    ipv4_packet.set_total_length();
-    ipv4_packet.set_identification();
-    ipv4_packet.set_flags();
-    ipv4_packet.set_fragment_offset();
-    ipv4_packet.set_ttl();
+
+    ipv4_packet.set_version(4);
+    ipv4_packet.set_header_length(5);
+    ipv4_packet.set_total_length(ipv4_packet.packet().len() as u16);
+    ipv4_packet.set_identification(257u16.to_be());
+    ipv4_packet.set_flags(Ipv4Flags::DontFragment);
+    ipv4_packet.set_fragment_offset(0);
+    ipv4_packet.set_ttl(64);
     ipv4_packet.set_next_level_protocol(IpNextHeaderProtocols::Icmp);
-    ipv4_packet.set_checksum();
-    ipv4_packet.set_source();
-    ipv4_packet.set_destination();
+
+    let checksum = ipv4_packet.packet();
+    ipv4_packet.set_checksum(util::checksum(checksum, 1));
+    ipv4_packet.set_source(Ipv4Addr::new(192, 168, 0, 1));
+    ipv4_packet.set_destination(destination);
 
     Ok(ipv4_packet)
 }
 
 fn create_packet_icmp<'a>(
     header: &'a mut [u8],
-) -> Result<MutableEchoRequestPacket<'a>, std::io::Error> {
-    // Código para crear y devolver el paquete MutableEchoRequestPacket
-
+) -> Result<MutableEchoRequestPacket<'_>, std::io::Error> {
     let mut icmp_packet = MutableEchoRequestPacket::new(header).unwrap();
     icmp_packet.set_icmp_type(IcmpTypes::EchoRequest);
     icmp_packet.set_icmp_code(IcmpCodes::NoCode);
@@ -67,4 +76,8 @@ fn create_packet_icmp<'a>(
     icmp_packet.set_sequence_number(1);
 
     Ok(icmp_packet)
+}
+
+pub fn ping(destination: Ipv4Addr) {
+    let protocol = transport::TransportChannelType::Layer4(Ipv4(IpNextHeaderProtocols::Icmp));
 }
