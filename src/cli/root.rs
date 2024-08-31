b64beefa372024bd4996dc81d20496813142776f
4c65a <pentestinglean@gmail.com>
2024-08-31 16:54:08 -0300

    Modify the root.rs file command

--- src/cli/root.rs
+++ src/cli/root.rs
@@ -9,14 +9,16 @@ pub fn root() -> Result<ArgMatches, Error> {
         .arg_required_else_help(true)
         // cap --interface enp2s0|any --count 10 --cap-send --cap-receive --protocol --port --address 192.168.0.0.1 --save cap.pcap
         .subcommand(
-            Command::new("cap").about("Capture packets from your network.").arg(
-                Arg::new("interface")
-                    .short('i')
-                    .long("interface")
-                    .value_name("INTERFACE")
-                    .action(ArgAction::Set)
-                    .help("Add interface type to capture packets."),
-            ),
+            Command::new("cap")
+                .about("Capture packets from your network.")
+                .arg(
+                    Arg::new("interface")
+                        .short('i')
+                        .long("interface")
+                        .value_name("INTERFACE")
+                        .action(ArgAction::Set)
+                        .help("Add interface type to capture packets."),
+                ),
         )
         // Interface --filter(type interface) --list(interface) --ip(ip private or public)
         .subcommand(
@@ -39,14 +41,15 @@ pub fn root() -> Result<ArgMatches, Error> {
         )
         // ping --address --min-send --count(packet) --ip6 --save
         .subcommand(
-            Command::new("ping").about("Ping is a network administration utility used to test the reachability of a host on an IP network. 
-                It sends an Internet Control Message Protocol (ICMP) echo request packet to the specified host and waits for an ICMP echo reply.").arg(
-                Arg::new("address")
-                    .short('a')
-                    .long("address")
-                    .help("Address server."),
-            ), //.arg(Arg::new("min-send").short('m').long("min-send").help(""))
-               //.arg(Arg::new("ip6").short('6').long("ip6").help("")),
+            Command::new("ping")
+                .about("Ping sends an Internet Control Message Protocol (ICMP).")
+                .arg(
+                    Arg::new("address")
+                        .short('a')
+                        .long("address")
+                        .help("Address server."),
+                ), //.arg(Arg::new("min-send").short('m').long("min-send").help(""))
+                   //.arg(Arg::new("ip6").short('6').long("ip6").help("")),
         )
         .get_matches();
     Ok(matches)
