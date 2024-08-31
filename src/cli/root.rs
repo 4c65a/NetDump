7c9feccbd0ff14169e4982d0e59d166ac903e0d7
4c65a <pentestinglean@gmail.com>
2024-08-31 16:51:41 -0300

    Modify the root.rs file command

--- src/cli/root.rs
+++ src/cli/root.rs
@@ -1,6 +1,5 @@
 use clap::{self, Arg, ArgAction, ArgMatches, Command, Error};
 
-//#[allow(dead_code)]
 pub fn root() -> Result<ArgMatches, Error> {
     let matches = Command::new("netdump")
         .author("Lean")
@@ -10,19 +9,19 @@ pub fn root() -> Result<ArgMatches, Error> {
         .arg_required_else_help(true)
         // cap --interface enp2s0|any --count 10 --cap-send --cap-receive --protocol --port --address 192.168.0.0.1 --save cap.pcap
         .subcommand(
-            Command::new("cap").about("").arg(
+            Command::new("cap").about("Capture packets from your network.").arg(
                 Arg::new("interface")
                     .short('i')
                     .long("interface")
                     .value_name("INTERFACE")
                     .action(ArgAction::Set)
-                    .help(""),
+                    .help("Add interface type to capture packets."),
             ),
         )
         // Interface --filter(type interface) --list(interface) --ip(ip private or public)
         .subcommand(
             Command::new("interface")
-                .about("")
+                .about("Get a list of available network interfaces.")
                 .arg(
                     Arg::new("list")
                         .short('l')
@@ -40,11 +39,12 @@ pub fn root() -> Result<ArgMatches, Error> {
         )
         // ping --address --min-send --count(packet) --ip6 --save
         .subcommand(
-            Command::new("ping").about("").arg(
+            Command::new("ping").about("Ping is a network administration utility used to test the reachability of a host on an IP network. 
+                It sends an Internet Control Message Protocol (ICMP) echo request packet to the specified host and waits for an ICMP echo reply.").arg(
                 Arg::new("address")
                     .short('a')
                     .long("address")
-                    .help("Ping server."),
+                    .help("Address server."),
             ), //.arg(Arg::new("min-send").short('m').long("min-send").help(""))
                //.arg(Arg::new("ip6").short('6').long("ip6").help("")),
         )
