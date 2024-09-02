3f323c0302d527e86328096fa539df8d18bc467c
4c65a <pentestinglean@gmail.com>
2024-09-02 12:55:42 -0300

    command configuration and code improvement filter in interfaces.

--- src/capture/interfaces.rs
+++ src/capture/interfaces.rs
@@ -1,23 +1,27 @@
 use pnet::datalink::interfaces;
 
-#[allow(dead_code)]
 pub fn filter_interfaces(name: &str) -> Result<(), std::io::Error> {
-    let inter = interfaces()
-        .into_iter()
-        .filter(|a| a.name == *name.to_string());
-    for interfaces in inter {
-        println!();
-        println!("Name: {}", interfaces.name);
-        println!("Description: {}", interfaces.description);
-        println!("Index: {}", interfaces.index);
-        println!("MAC: {:?}", interfaces.mac);
-        println!("IPs: {:?}", interfaces.ips);
-        println!("Flags: {}", interfaces.flags);
-        println!();
+    let inter = interfaces();
+    let names = inter.into_iter().find(|a| a.name == *name.to_string());
+
+    match names {
+        Some(interface) => {
+            println!();
+            println!("Name: {}", interface.name);
+            println!("Description: {}", interface.description);
+            println!("Index: {}", interface.index);
+            println!("MAC: {:?}", interface.mac);
+            println!("IPs: {:?}", interface.ips);
+            println!("Flags: {}", interface.flags);
+            println!();
+        }
+        None => {
+            println!("Interface '{}' not found.", name);
+        }
     }
     Ok(())
 }
-#[allow(dead_code)]
+
 pub fn index_interface() -> Result<(), std::io::Error> {
     let inter = interfaces();
     //let interface = vec![inter];
