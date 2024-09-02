be725e9767088a69d35d5e8f385b7d58ac297336
4c65a <pentestinglean@gmail.com>
2024-09-02 20:32:10 -0300

    .

--- src/route/create_packet.rs
+++ src/route/create_packet.rs
@@ -18,32 +18,67 @@ use std::{
     net::{Ipv4Addr, Ipv6Addr},
 };
 
+/*
+
+    IPv4(icmp4):
+    +------------------+-----------------+-----------------+
+    | 20 bytes (IPv4)  | 8 bytes (ICMP)  | 32 bytes (Payload)|
+    +------------------+-----------------+-----------------+
+    |             Total: 60 bytes                        |
+    +---------------------------------------------------+
+
+    IPv6(icmp6):
+    +------------------+-----------------+-----------------+
+    | 40 bytes (IPv6)  | 8 bytes (ICMPv6)| 32 bytes (Payload)|
+    +------------------+-----------------+-----------------+
+    |             Total: 80 bytes                        |
+    +---------------------------------------------------+
+
+
+*/
+
 const PAYLOAD_ICMP: usize = 56;
 const ICMP_SIZE: usize = 8;
 const IPV4_SIZE: usize = 20;
 const TOTAL_LENGTH_SIZE: usize = IPV4_SIZE + ICMP_SIZE + PAYLOAD_ICMP;
+
+const IPV6_SIZE: usize = 40;
+const TOTAL_LENGTH_SIZE_IPV6: usize = IPV6_SIZE + ICMP_SIZE + PAYLOAD_ICMP;
+
 #[allow(dead_code)]
 pub fn handle_packet(destination: Ipv4Addr) -> Result<Vec<u8>, io::Error> {
     // Crear y configurar el paquete ICMP
     let mut icmp_packet: [u8; ICMP_SIZE] = [0; ICMP_SIZE];
     let mut icmp = MutableEchoRequestPacket::new(&mut icmp_packet).unwrap();
     create_packet_icmp(&mut icmp);
+
     // Calcular el checksum de Icmp
     let icmp_checksum = util::checksum(icmp.packet(), 1);
     icmp.set_checksum(icmp_checksum);
+
     // Crear y configurar el paquete IPv4
     let mut ip_packet: [u8; TOTAL_LENGTH_SIZE] = [0; TOTAL_LENGTH_SIZE];
     let mut ipv4 = MutableIpv4Packet::new(&mut ip_packet).unwrap();
     ipv4_create_packet(&mut ipv4, destination);
+
     //Establecer el tamaño total
     ipv4.set_total_length((TOTAL_LENGTH_SIZE + icmp.packet().len()) as u16);
+
     // Establecer la carga útil del IPv4 con el paquete ICMP
     ipv4.set_payload(icmp.packet());
+
     // Calcular el checksum del IPv4
     let ipv4_checksum = util::checksum(ipv4.packet(), 1);
     ipv4.set_checksum(ipv4_checksum);
     Ok(ipv4.packet().to_vec())
 }
+
+#[allow(dead_code)]
+pub fn handle_packet_ipv6(destinantion: Ipv6Addr) -> Result<Vec<u8>, io::Error> {
+    // Create packet
+    let mut icmp_packet: [u8; ICMP_SIZE] = [0; ICMP_SIZE];
+}
+
 #[allow(dead_code)]
 pub fn ipv4_create_packet(ipv4_packet: &mut MutableIpv4Packet, destination: Ipv4Addr) {
     ipv4_packet.set_version(4);
