a35b881b3f045edbb90de8bedade04b17803e697
4c65a <pentestinglean@gmail.com>
2025-01-01 20:32:08 -0300

    Update handle packet ipv6

--- src/route/create_packet.rs
+++ src/route/create_packet.rs
@@ -15,7 +15,6 @@ use std::{
     net::{Ipv4Addr, Ipv6Addr},
 };
 
-
 /*
 
     IPv4(icmp4):
@@ -40,17 +39,10 @@ const ICMP_SIZE: usize = 8;
 const IPV4_SIZE: usize = 20;
 const TOTAL_LENGTH_SIZE: usize = IPV4_SIZE + ICMP_SIZE + PAYLOAD_ICMP;
 
-const IPV6_SIZE: usize = 40;
+const IPV6_SIZE: usize = 20;
 const TOTAL_LENGTH_SIZE_IPV6: usize = IPV6_SIZE + ICMP_SIZE + PAYLOAD_ICMP;
 
-
-
-pub fn handle_packet(
-    destination: Ipv4Addr,
-    ttl: u8,
-   
-) -> Result<Vec<u8>, io::Error> {
-
+pub fn handle_packet(destination: Ipv4Addr, ttl: u8) -> Result<Vec<u8>, io::Error> {
     // Crear y configurar el paquete ICMP
     let mut icmp_packet: [u8; ICMP_SIZE] = [0; ICMP_SIZE];
     let mut icmp =
@@ -64,7 +56,7 @@ pub fn handle_packet(
     // Crear y configurar el paquete IPv4
     let mut ip_packet: [u8; TOTAL_LENGTH_SIZE] = [0; TOTAL_LENGTH_SIZE];
     let mut ipv4 = MutableIpv4Packet::new(&mut ip_packet).unwrap();
-    ipv4_create_packet(&mut ipv4, destination,ttl);
+    ipv4_create_packet(&mut ipv4, destination, ttl);
 
     //Establecer el tamaño total
     ipv4.set_total_length((TOTAL_LENGTH_SIZE + icmp.packet().len()) as u16);
@@ -77,7 +69,6 @@ pub fn handle_packet(
     ipv4.set_checksum(ipv4_checksum);
     Ok(ipv4.packet().to_vec())
 }
-
 
 pub fn handle_packet_ipv6(destinantion: Ipv6Addr) -> Result<Vec<u8>, io::Error> {
     // Create packet
@@ -92,7 +83,12 @@ pub fn handle_packet_ipv6(destinantion: Ipv6Addr) -> Result<Vec<u8>, io::Error> 
     let mut ipv6 = MutableIpv6Packet::new(&mut ip6_packet).unwrap();
     ipv6_create_packet(&mut ipv6, destinantion);
 
+    ipv6.set_payload_length((TOTAL_LENGTH_SIZE_IPV6 + icmp6.packet().len()) as u16);
+    
+    // Establecer la carga útil del IPv6 con el paquete ICMP
     ipv6.set_payload(icmp6.packet());
+
+  
 
     Ok(ipv6.packet().to_vec())
 }
@@ -147,7 +143,6 @@ pub fn ipv4_create_packet(ipv4_packet: &mut MutableIpv4Packet, destination: Ipv4
     +-----------------------------------------+
 */
 
-
 pub fn ipv6_create_packet(ipv6_packet: &mut MutableIpv6Packet, destinantion: Ipv6Addr) {
     ipv6_packet.set_version(6);
     ipv6_packet.set_next_header(IpNextHeaderProtocols::Icmpv6);
@@ -168,7 +163,6 @@ pub fn ipv6_create_packet(ipv6_packet: &mut MutableIpv6Packet, destinantion: Ipv
     +-----------------------------------------+
 */
 
-
 fn create_packet_icmp(
     echo_packet: &mut pnet::packet::icmp::echo_request::MutableEchoRequestPacket,
 ) {
@@ -177,7 +171,6 @@ fn create_packet_icmp(
     echo_packet.set_identifier(1);
     echo_packet.set_sequence_number(1);
 }
-
 
 fn create_packet_icmp6(
     echo_packet: &mut pnet::packet::icmpv6::echo_request::MutableEchoRequestPacket,
